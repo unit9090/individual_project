065c7162d8981308a4b7b721b00ba5ddb00f6f8f
unit9090 <unit9099@gmail.com>
2023-04-21 15:23:00 +0900

    oracle 접속 수정

--- src/edu/java/ojdbc/OracleConnect.java
+++ src/edu/java/ojdbc/OracleConnect.java
@@ -2,7 +2,9 @@ package edu.java.ojdbc;
 
 public interface OracleConnect {
 	// Oracle DB 접속 URL
-	String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+//	String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	String URL = "jdbc:oracle:thin:@169.254.208.67:1521:xe";
+	
 	
 	// Oracle DB 접속 계정
 	String USER = "scott";
