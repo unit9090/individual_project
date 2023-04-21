e103eaab2ba19f8c57dfb7cfdb168d7de5cdb769
unit9090 <unit9099@gmail.com>
2023-04-21 15:25:59 +0900

    0

--- src/edu/java/ojdbc/OracleConnect.java
+++ src/edu/java/ojdbc/OracleConnect.java
@@ -2,8 +2,8 @@ package edu.java.ojdbc;
 
 public interface OracleConnect {
 	// Oracle DB 접속 URL
-//	String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-	String URL = "jdbc:oracle:thin:@169.254.208.67:1521:xe";
+	String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+//	String URL = "jdbc:oracle:thin:@169.254.208.67:1521:xe";
 	
 	
 	// Oracle DB 접속 계정
